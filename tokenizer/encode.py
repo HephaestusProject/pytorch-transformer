6c28bc722b20725cddc2ce27cc23a6f91609fcb4
inmoonlight <mjihyung@gmail.com>
2020-07-28 17:27:02 +0900

    Feat: add tokenizer

---
+++ tokenizer/encode.py
@@ -0,0 +1,20 @@
+from tokenizers import SentencePieceBPETokenizer
+from tqdm.auto import tqdm
+
+from utils import root_dir, dataset_config, tokenizer_config, read_lines
+
+source_train = read_lines(f'{root_dir}/{dataset_config.raw.source_train}')
+target_train = read_lines(f'{root_dir}/{dataset_config.raw.target_train}')
+assert len(source_train) == len(target_train)
+
+tokenizer = SentencePieceBPETokenizer(f'{tokenizer_config.tokenizer_name}-vocab.json', f'{tokenizer_config.tokenizer_name}-merges.txt')
+
+results = tokenizer.encode_batch(source_train)
+with open(f'{root_dir}/{dataset_config.tokenized.source_train}', 'w') as f:
+    for line in tqdm(results, desc="source"):
+        f.write(' '.join(line.tokens) + '\n')
+
+results = tokenizer.encode_batch(target_train)
+with open(f'{root_dir}/{dataset_config.tokenized.target_train}', 'w') as f:
+    for line in tqdm(results, desc="target"):
+        f.write(' '.join(line.tokens) + '\n')
