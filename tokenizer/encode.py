22e494947bd4a678e65989b07a6adc766db309e0
inmoonlight <mjihyung@gmail.com>
2020-07-30 00:02:05 +0900

    Feat: split encode and tokenize

--- tokenizer/encode.py
+++ tokenizer/encode.py
@@ -1,23 +1,94 @@
-from tokenizers import SentencePieceBPETokenizer
-from tqdm.auto import tqdm
+from tokenizers import Tokenizer, SentencePieceBPETokenizer
+from typing import List
 
-from utils import dataset_config, read_lines, root_dir, tokenizer_config
+from utils import root_dir, tokenizer_config
 
-source_train = read_lines(f"{root_dir}/{dataset_config.raw.source_train}")
-target_train = read_lines(f"{root_dir}/{dataset_config.raw.target_train}")
-assert len(source_train) == len(target_train)
 
-tokenizer = SentencePieceBPETokenizer(
-    f"{tokenizer_config.tokenizer_name}-vocab.json",
-    f"{tokenizer_config.tokenizer_name}-merges.txt",
-)
+def load_tokenizer():
+    tokenizer = SentencePieceBPETokenizer(
+        f"{root_dir}/tokenizer/{tokenizer_config.tokenizer_name}-vocab.json",
+        f"{root_dir}/tokenizer/{tokenizer_config.tokenizer_name}-merges.txt",
+    )
+    return tokenizer
 
-results = tokenizer.encode_batch(source_train)
-with open(f"{root_dir}/{dataset_config.tokenized.source_train}", "w") as f:
-    for line in tqdm(results, desc="source"):
-        f.write(" ".join(line.tokens) + "\n")
 
-results = tokenizer.encode_batch(target_train)
-with open(f"{root_dir}/{dataset_config.tokenized.target_train}", "w") as f:
-    for line in tqdm(results, desc="target"):
-        f.write(" ".join(line.tokens) + "\n")
+def encode(tokenizer: Tokenizer, line: str):
+    """Encode a single string line.
+
+    Args:
+        tokenizer: trained huggingface tokenizer
+        line: raw string
+
+    Returns:
+        encoded_line: encode string
+
+    Example:
+    >>> tokenizer = load_tokenizer()
+    >>> line = "Hello"
+    >>> encode(tokenizer, line)
+    Encoding(num_tokens=2, attributes=[ids, type_ids, tokens, offsets, attention_mask, special_tokens_mask, overflowing])
+    """
+    return tokenizer.encode(line)
+
+
+def encode_batch(tokenizer: Tokenizer, lines: List[str]):
+    """Encode multiple string lines.
+
+    Args:
+        tokenizer: trained huggingface tokenizer
+        lines: list of raw string
+
+    Returns:
+        encoded_lines: list of encoded string
+
+    Example:
+    >>> tokenizer = load_tokenizer()
+    >>> lines = ['Hello', 'Jihyung']
+    >>> encode_batch(tokenizer, lines)
+    [Encoding(num_tokens=2, attributes=[ids, type_ids, tokens, offsets, attention_mask, special_tokens_mask, overflowing]),
+     Encoding(num_tokens=4, attributes=[ids, type_ids, tokens, offsets, attention_mask, special_tokens_mask, overflowing])]
+    """
+    return tokenizer.encode_batch(lines)
+
+
+def tokenize(tokenizer: Tokenizer, line: str):
+    """Tokenize a single string line.
+
+    Args:
+        tokenizer: trained huggingface tokenizer
+        line: raw string
+
+    Returns:
+        tokenized: list of tokens
+
+    Example:
+    >>> tokenizer = load_tokenizer()
+    >>> line = 'Hello"
+    >>> tokenize(tokenizer, line)
+    ['▁He', 'llo']
+    """
+    return tokenizer.encode(line).tokens
+
+
+def tokenize_batch(tokenizer: Tokenizer, lines: List[str]):
+    """Tokenize multiple string lines.
+
+    Args:
+        tokenizer: trained huggingface tokenizer
+        lines: list of raw string
+
+    Returns:
+        tokenized: list of list of tokens
+
+    Example:
+    >>> tokenizer = load_tokenizer()
+    >>> lines = ['Hello', 'Jihyung']
+    >>> tokenizee_batch(tokenizer, lines)
+    [['▁He', 'llo'], ['▁J', 'ih', 'y', 'ung']]
+    """
+    encoded_results = tokenizer.encode_batch(lines)
+    # TODO: multiprocessing
+    tokenized = []
+    for encoded_result in encoded_results:
+        tokenized.append(encoded_result.tokens)
+    return tokenized
