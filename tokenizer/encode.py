bed039f8ff2efb6f62be1a4dec7665487e76944f
inmoonlight <mjihyung@gmail.com>
2020-07-29 19:46:43 +0900

    Style: reflect isort and black

--- tokenizer/encode.py
+++ tokenizer/encode.py
@@ -1,20 +1,23 @@
 from tokenizers import SentencePieceBPETokenizer
 from tqdm.auto import tqdm
 
-from utils import root_dir, dataset_config, tokenizer_config, read_lines
+from utils import dataset_config, read_lines, root_dir, tokenizer_config
 
-source_train = read_lines(f'{root_dir}/{dataset_config.raw.source_train}')
-target_train = read_lines(f'{root_dir}/{dataset_config.raw.target_train}')
+source_train = read_lines(f"{root_dir}/{dataset_config.raw.source_train}")
+target_train = read_lines(f"{root_dir}/{dataset_config.raw.target_train}")
 assert len(source_train) == len(target_train)
 
-tokenizer = SentencePieceBPETokenizer(f'{tokenizer_config.tokenizer_name}-vocab.json', f'{tokenizer_config.tokenizer_name}-merges.txt')
+tokenizer = SentencePieceBPETokenizer(
+    f"{tokenizer_config.tokenizer_name}-vocab.json",
+    f"{tokenizer_config.tokenizer_name}-merges.txt",
+)
 
 results = tokenizer.encode_batch(source_train)
-with open(f'{root_dir}/{dataset_config.tokenized.source_train}', 'w') as f:
+with open(f"{root_dir}/{dataset_config.tokenized.source_train}", "w") as f:
     for line in tqdm(results, desc="source"):
-        f.write(' '.join(line.tokens) + '\n')
+        f.write(" ".join(line.tokens) + "\n")
 
 results = tokenizer.encode_batch(target_train)
-with open(f'{root_dir}/{dataset_config.tokenized.target_train}', 'w') as f:
+with open(f"{root_dir}/{dataset_config.tokenized.target_train}", "w") as f:
     for line in tqdm(results, desc="target"):
-        f.write(' '.join(line.tokens) + '\n')
+        f.write(" ".join(line.tokens) + "\n")
--- tokenizer/utils.py
+++ tokenizer/utils.py
@@ -2,13 +2,15 @@ import os
 
 from omegaconf import OmegaConf
 
-root_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), '../')
-config_dir = f'{root_dir}/configs'
+root_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), "../")
+config_dir = f"{root_dir}/configs"
 
-dataset_config = OmegaConf.load(f'{config_dir}/dataset/wmt14.de-en.yaml')
-tokenizer_config = OmegaConf.load(f'{config_dir}/tokenizer/sentencepiece_bpe_wmt14_deen.yaml')
+dataset_config = OmegaConf.load(f"{config_dir}/dataset/wmt14.de-en.yaml")
+tokenizer_config = OmegaConf.load(
+    f"{config_dir}/tokenizer/sentencepiece_bpe_wmt14_deen.yaml"
+)
 
 
 def read_lines(path):
-    with open(path, encoding='utf-8', newline='\n') as f:
+    with open(path, encoding="utf-8", newline="\n") as f:
         return f.readlines()
