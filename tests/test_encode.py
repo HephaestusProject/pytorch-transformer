cb085a61ef5f7eaf06114cde8edee663bacf1ac0
inmoonlight <mjihyung@gmail.com>
2020-08-15 15:11:52 +0900

    Test: add encode test

--- tests/test_encode.py
+++ tests/test_encode.py
@@ -1,8 +1,24 @@
 import pytest  # noqa: F401
 
-from tokenizer.encode import load_tokenizer, tokenize, tokenize_batch
+import tokenizers
+from tokenizer.encode import load_tokenizer, encode, encode_batch, tokenize, tokenize_batch
 
 tokenizer = load_tokenizer()
+
+
+def test_encode():
+    line = "Hello"
+    result = encode(tokenizer, line)
+    assert type(result) == tokenizers.Encoding
+    assert result.tokens == ["▁He", "llo"]
+
+
+def test_encode_batch():
+    lines = ["Hello", "Jihyung"]
+    results = encode_batch(tokenizer, lines)
+    assert type(results) == list
+    assert results[0].tokens == ["▁He", "llo"]
+    assert results[1].tokens == ["▁J", "ih", "y", "ung"]
 
 
 def test_tokenize():
