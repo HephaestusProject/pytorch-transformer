bc63ab8c3f6d0dbf5ab3ff503ae0c065e92ba1ff
inmoonlight <mjihyung@gmail.com>
2020-09-06 14:00:25 +0900

    refactor: tokenizer
    Remove and update script

--- tokenizer/train_bpe.py
+++ tokenizer/train_bpe.py
@@ -1,12 +1,19 @@
-from tokenizers import SentencePieceBPETokenizer
+from pathlib import Path
 
-from utils import dataset_config, root_dir, tokenizer_config
+from tokenizers import SentencePieceBPETokenizer
+from omegaconf import OmegaConf
+
+root_dir = Path('..')
+config_dir = root_dir / 'configs'
+dataset_config = OmegaConf.load(config_dir / 'dataset' / 'wmt14.de-en.yaml')
+tokenizer_config = OmegaConf.load(config_dir / 'tokenizer' / 'sentencepiece_bpe_wmt14_deen.yaml')
+
 
 tokenizer = SentencePieceBPETokenizer()
 tokenizer.train(
     [
-        f"{root_dir}/{dataset_config.path.source_train}",
-        f"{root_dir}/{dataset_config.path.target_train}",
+        str(root_dir / dataset_config.path.source_train),
+        str(root_dir / dataset_config.path.target_train)
     ],
     vocab_size=tokenizer_config.vocab_size,
     min_frequency=tokenizer_config.min_frequency,
@@ -14,5 +21,5 @@ tokenizer.train(
     limit_alphabet=tokenizer_config.limit_alphabet,
 )
 tokenizer.save_model(
-    directory=f"{root_dir}/tokenizer", name=tokenizer_config.tokenizer_name
+    directory='.', name=tokenizer_config.tokenizer_name
 )
