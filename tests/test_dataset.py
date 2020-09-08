8412b69d1de1a7efc1680f41d040cff128439b97
inmoonlight <mjihyung@gmail.com>
2020-09-09 02:32:35 +0900

    test: deal with long sentence

--- tests/test_dataset.py
+++ tests/test_dataset.py
@@ -55,9 +55,9 @@ def test_len(langpair, source_lines, target_lines):
 )
 def test_getitem(langpair, source_lines, target_lines):
     ds = WMT14Dataset(langpair, source_lines, target_lines)
-    source_encode_pad_test, target_encode_pad_test = ds[0]
-    assert source_encode_pad_test.size() == target_encode_pad_test.size()
-    assert source_encode_pad_test.size()[0] == ds.model_config.max_len
+    for source_encode_pad_test, target_encode_pad_test in ds:
+        assert source_encode_pad_test.size() == target_encode_pad_test.size()
+        assert source_encode_pad_test.size()[0] == ds.model_config.max_len
 
 
 @pytest.mark.parametrize("langpair", ["de-en"])
@@ -81,15 +81,14 @@ def test_getitem(langpair, source_lines, target_lines):
 )
 def test_encode(langpair, source_lines, target_lines):
     ds = WMT14Dataset(langpair, source_lines, target_lines)
-    source_encode_test, target_encode_test = ds._encode(
-        source_lines[0], target_lines[0]
-    )
-    bos = ds.tokenizer.token_to_id("<bos>")
-    eos = ds.tokenizer.token_to_id("<eos>")
-    assert target_encode_test[0] == bos
-    assert target_encode_test[-1] == eos
-    assert isinstance(source_encode_test, list)
-    assert isinstance(source_encode_test[0], int)
+    for source_line, target_line in zip(source_lines, target_lines):
+        source_encode_test, target_encode_test = ds._encode(source_lines, target_line)
+        bos = ds.tokenizer.token_to_id("<bos>")
+        eos = ds.tokenizer.token_to_id("<eos>")
+        assert target_encode_test[0] == bos
+        assert target_encode_test[-1] == eos
+        assert isinstance(source_encode_test, list)
+        assert isinstance(source_encode_test[0], int)
 
 
 @pytest.mark.parametrize("langpair", ["de-en"])
@@ -113,8 +112,9 @@ def test_encode(langpair, source_lines, target_lines):
 )
 def test_collate(langpair, source_lines, target_lines):
     ds = WMT14Dataset(langpair, source_lines, target_lines)
-    source_encode_pad_test, target_encode_pad_test = ds.collate(
-        source_lines[0], target_lines[0]
-    )
-    assert source_encode_pad_test.size() == target_encode_pad_test.size()
-    assert source_encode_pad_test.size()[0] == ds.model_config.max_len
+    for source_line, target_line in zip(source_lines, target_lines):
+        source_encode_pad_test, target_encode_pad_test = ds.collate(
+            source_line, target_line
+        )
+        assert source_encode_pad_test.size() == target_encode_pad_test.size()
+        assert source_encode_pad_test.size()[0] == ds.model_config.max_len
