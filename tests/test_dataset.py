5c6549b1973c3ad1be1c9f416812fd44813d2373
inmoonlight <mjihyung@gmail.com>
2020-09-11 06:19:41 +0900

    test: refactor test

--- tests/test_dataset.py
+++ tests/test_dataset.py
@@ -2,57 +2,32 @@ import pytest  # noqa: E902
 from src.dataset import WMT14Dataset
 
 
-@pytest.mark.parametrize("langpair", ["en-ko"])
-@pytest.mark.parametrize("source_lines", ["Hello"])
-@pytest.mark.parametrize("target_lines", ["안녕하세요"])
+test_langpair_exception_input = [
+    # (langpair, source_lines, target_lines)
+    ("en-ko", "Hello", "안녕하세요"),
+    ("ko-ja", "감사합니다", "ありがとうございました"),
+]
+
+
+@pytest.mark.parametrize("langpair, source_lines, target_lines", test_langpair_exception_input)
 def test_langpair_exception(langpair, source_lines, target_lines):
     with pytest.raises(NotImplementedError):
         WMT14Dataset(langpair, source_lines, target_lines)
 
 
-@pytest.mark.parametrize("langpair", ["de-en"])
-@pytest.mark.parametrize(
-    "source_lines",
-    [
-        [
-            "Der Bau und die Reparatur der Autostraßen...",
-            "Dies ist ein sehr sehr langer Satz. Dies ist ein sehr sehr langer Satz. Dies ist ein sehr sehr langer Satz. Dies ist ein sehr sehr langer Satz. Dies ist ein sehr sehr langer Satz. Dies ist ein sehr sehr langer Satz. Dies ist ein sehr sehr langer Satz. Dies ist ein sehr sehr langer Satz. Dies ist ein sehr sehr langer Satz. Dies ist ein sehr sehr langer Satz. Dies ist ein sehr sehr langer Satz. Dies ist ein sehr sehr langer Satz. Dies ist ein sehr sehr langer Satz. Dies ist ein sehr sehr langer Satz. Dies ist ein sehr sehr langer Satz.",
-        ]
-    ],
-)
-@pytest.mark.parametrize(
-    "target_lines",
-    [
-        [
-            "Construction and repair of highways and...",
-            "This is a very very long sentence. This is a very very long sentence. This is a very very long sentence. This is a very very long sentence. This is a very very long sentence. This is a very very long sentence. This is a very very long sentence. This is a very very long sentence. This is a very very long sentence. This is a very very long sentence. This is a very very long sentence. This is a very very long sentence. This is a very very long sentence. This is a very very long sentence. This is a very very long sentence.",
-        ]
-    ],
-)
+test_input = [
+    # (langpair, source_lines, target_lines)
+    ("de-en", ["Der Bau und die Reparatur der Autostraßen...", "Dies ist ein sehr sehr langer Satz. Dies ist ein sehr sehr langer Satz. Dies ist ein sehr sehr langer Satz. Dies ist ein sehr sehr langer Satz. Dies ist ein sehr sehr langer Satz. Dies ist ein sehr sehr langer Satz. Dies ist ein sehr sehr langer Satz. Dies ist ein sehr sehr langer Satz. Dies ist ein sehr sehr langer Satz. Dies ist ein sehr sehr langer Satz. Dies ist ein sehr sehr langer Satz. Dies ist ein sehr sehr langer Satz. Dies ist ein sehr sehr langer Satz. Dies ist ein sehr sehr langer Satz. Dies ist ein sehr sehr langer Satz."], ["Construction and repair of highways and...", "This is a very very long sentence. This is a very very long sentence. This is a very very long sentence. This is a very very long sentence. This is a very very long sentence. This is a very very long sentence. This is a very very long sentence. This is a very very long sentence. This is a very very long sentence. This is a very very long sentence. This is a very very long sentence. This is a very very long sentence. This is a very very long sentence. This is a very very long sentence. This is a very very long sentence."])
+]
+
+
+@pytest.mark.parametrize("langpair, source_lines, target_lines", test_input)
 def test_len(langpair, source_lines, target_lines):
     ds = WMT14Dataset(langpair, source_lines, target_lines)
     assert len(ds.source_lines) == len(ds)
 
 
-@pytest.mark.parametrize("langpair", ["de-en"])
-@pytest.mark.parametrize(
-    "source_lines",
-    [
-        [
-            "Der Bau und die Reparatur der Autostraßen...",
-            "Dies ist ein sehr sehr langer Satz. Dies ist ein sehr sehr langer Satz. Dies ist ein sehr sehr langer Satz. Dies ist ein sehr sehr langer Satz. Dies ist ein sehr sehr langer Satz. Dies ist ein sehr sehr langer Satz. Dies ist ein sehr sehr langer Satz. Dies ist ein sehr sehr langer Satz. Dies ist ein sehr sehr langer Satz. Dies ist ein sehr sehr langer Satz. Dies ist ein sehr sehr langer Satz. Dies ist ein sehr sehr langer Satz. Dies ist ein sehr sehr langer Satz. Dies ist ein sehr sehr langer Satz. Dies ist ein sehr sehr langer Satz.",
-        ]
-    ],
-)
-@pytest.mark.parametrize(
-    "target_lines",
-    [
-        [
-            "Construction and repair of highways and...",
-            "This is a very very long sentence. This is a very very long sentence. This is a very very long sentence. This is a very very long sentence. This is a very very long sentence. This is a very very long sentence. This is a very very long sentence. This is a very very long sentence. This is a very very long sentence. This is a very very long sentence. This is a very very long sentence. This is a very very long sentence. This is a very very long sentence. This is a very very long sentence. This is a very very long sentence.",
-        ]
-    ],
-)
+@pytest.mark.parametrize("langpair, source_lines, target_lines", test_input)
 def test_getitem(langpair, source_lines, target_lines):
     ds = WMT14Dataset(langpair, source_lines, target_lines)
     for source_encode_pad_test, target_encode_pad_test in ds:
@@ -60,25 +35,7 @@ def test_getitem(langpair, source_lines, target_lines):
         assert source_encode_pad_test.size()[0] == ds.model_config.max_len
 
 
-@pytest.mark.parametrize("langpair", ["de-en"])
-@pytest.mark.parametrize(
-    "source_lines",
-    [
-        [
-            "Der Bau und die Reparatur der Autostraßen...",
-            "Dies ist ein sehr sehr langer Satz. Dies ist ein sehr sehr langer Satz. Dies ist ein sehr sehr langer Satz. Dies ist ein sehr sehr langer Satz. Dies ist ein sehr sehr langer Satz. Dies ist ein sehr sehr langer Satz. Dies ist ein sehr sehr langer Satz. Dies ist ein sehr sehr langer Satz. Dies ist ein sehr sehr langer Satz. Dies ist ein sehr sehr langer Satz. Dies ist ein sehr sehr langer Satz. Dies ist ein sehr sehr langer Satz. Dies ist ein sehr sehr langer Satz. Dies ist ein sehr sehr langer Satz. Dies ist ein sehr sehr langer Satz.",
-        ]
-    ],
-)
-@pytest.mark.parametrize(
-    "target_lines",
-    [
-        [
-            "Construction and repair of highways and...",
-            "This is a very very long sentence. This is a very very long sentence. This is a very very long sentence. This is a very very long sentence. This is a very very long sentence. This is a very very long sentence. This is a very very long sentence. This is a very very long sentence. This is a very very long sentence. This is a very very long sentence. This is a very very long sentence. This is a very very long sentence. This is a very very long sentence. This is a very very long sentence. This is a very very long sentence.",
-        ]
-    ],
-)
+@pytest.mark.parametrize("langpair, source_lines, target_lines", test_input)
 def test_encode(langpair, source_lines, target_lines):
     ds = WMT14Dataset(langpair, source_lines, target_lines)
     for source_line, target_line in zip(source_lines, target_lines):
@@ -91,25 +48,7 @@ def test_encode(langpair, source_lines, target_lines):
         assert isinstance(source_encode_test[0], int)
 
 
-@pytest.mark.parametrize("langpair", ["de-en"])
-@pytest.mark.parametrize(
-    "source_lines",
-    [
-        [
-            "Der Bau und die Reparatur der Autostraßen...",
-            "Dies ist ein sehr sehr langer Satz. Dies ist ein sehr sehr langer Satz. Dies ist ein sehr sehr langer Satz. Dies ist ein sehr sehr langer Satz. Dies ist ein sehr sehr langer Satz. Dies ist ein sehr sehr langer Satz. Dies ist ein sehr sehr langer Satz. Dies ist ein sehr sehr langer Satz. Dies ist ein sehr sehr langer Satz. Dies ist ein sehr sehr langer Satz. Dies ist ein sehr sehr langer Satz. Dies ist ein sehr sehr langer Satz. Dies ist ein sehr sehr langer Satz. Dies ist ein sehr sehr langer Satz. Dies ist ein sehr sehr langer Satz.",
-        ]
-    ],
-)
-@pytest.mark.parametrize(
-    "target_lines",
-    [
-        [
-            "Construction and repair of highways and...",
-            "This is a very very long sentence. This is a very very long sentence. This is a very very long sentence. This is a very very long sentence. This is a very very long sentence. This is a very very long sentence. This is a very very long sentence. This is a very very long sentence. This is a very very long sentence. This is a very very long sentence. This is a very very long sentence. This is a very very long sentence. This is a very very long sentence. This is a very very long sentence. This is a very very long sentence.",
-        ]
-    ],
-)
+@pytest.mark.parametrize("langpair, source_lines, target_lines", test_input)
 def test_collate(langpair, source_lines, target_lines):
     ds = WMT14Dataset(langpair, source_lines, target_lines)
     for source_line, target_line in zip(source_lines, target_lines):
