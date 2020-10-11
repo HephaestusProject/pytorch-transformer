df282141732bb3529e7b1d17e4ff822085766da3
inmoonlight <mjihyung@gmail.com>
2020-10-11 12:00:08 +0900

    feat: add training code

--- src/model/transformer.py
+++ src/model/transformer.py
@@ -1,3 +1,5 @@
+import torch
+from pytorch_lightning import LightningModule
 from torch import Tensor, nn
 
 from src.model.decoder import Decoder
@@ -5,16 +7,16 @@ from src.model.encoder import Encoder
 from src.utils import Config
 
 
-class Transformer(nn.Module):
+class Transformer(LightningModule):
     """Transformer Model"""
 
     def __init__(self, langpair: str, is_base: bool = True) -> None:
         super().__init__()
-        configs = Config()
-        configs.add_tokenizer(langpair)
-        configs.add_model(is_base)
-        dim_model: int = configs.model.model_params.dim_model
-        vocab_size = configs.tokenizer.vocab_size
+        self.configs = Config()
+        self.configs.add_tokenizer(langpair)
+        self.configs.add_model(is_base)
+        dim_model: int = self.configs.model.model_params.dim_model
+        vocab_size = self.configs.tokenizer.vocab_size
 
         self.encoder = Encoder(langpair)
         self.decoder = Decoder(langpair)
@@ -33,3 +35,32 @@ class Transformer(nn.Module):
         )
         output = self.linear(target_emb)
         return output
+
+    def training_step(self, batch):
+        source = batch.source
+        target = batch.target
+        target_hat = self(source.padded_token, source.mask, target.padded_token, target.mask)
+        loss = nn.CrossEntropyLoss(target_hat, target)  # TODO: fix
+        return loss
+
+    def test_step(self, batch):
+        source = batch.source
+        target = batch.target
+        target_hat = self(source.padded_token, source.mask, target.padded_token, target.mask)
+        loss = nn.CrossEntropyLoss(target_hat, target)  # TODO: fix
+        return loss
+
+    def configure_optimizers(self):
+
+        def _inverse_sqrt_warmup_scheduler(dim_model, current_step, warmup_steps):
+            if current_step == 0:
+                return 0
+            else:
+                option_1 = current_step ** (-0.5)
+                option_2 = current_step * (warmup_steps ** (-1.5))
+                return dim_model ** (-0.5) * min(option_1, option_2)
+
+        optimizer = torch.optim.Adam(self.parameters(), betas=(self.configs.model.train_hparams.beta_1, self.configs.model.train_hparams.beta_2), eps=self.configs.model.train_hparams.eps)
+        lr_lambda = lambda step: _inverse_sqrt_warmup_scheduler(self.configs.model.model_params.dim_model, step, self.configs.model.train_hparams.warmup_steps)  # noqa: E731
+        scheduler = torch.optim.lr_scheduler.LambdaLR(optimizer, lr_lambda=lr_lambda)
+        return [optimizer], [scheduler]
--- train.py
+++ train.py
@@ -1,4 +1,55 @@
-"""
-    This script was made by Nick at 19/07/20.
-    To implement code for training your model.
-"""
+import argparse
+from datetime import datetime
+from pathlib import Path
+
+import torch
+from torch import nn
+from pytorch_lightning import seed_everything, Trainer
+from pytorch_lightning.loggers import WandbLogger
+from pytorch_lightning.callbacks import ModelCheckpoint
+
+from src.dataloader import WMT14DataLoader
+from src.utils import Config
+from src.model.transformer import Transformer
+
+
+def train(langpair: str, model_type: str):
+    is_base = model_type == "base"
+
+    config = Config()
+    config.add_model(is_base)
+
+    seed_everything(seed=42)
+
+    save_dir = "results/"
+    model_name = f"transformer-{langpair}-{model_type}"
+    wandb_logger = WandbLogger(model_name, save_dir=save_dir, version=f"version-{datetime.now().strftime('%d-%m-%Y--%H-%M-%S')}")
+    checkpoint_path = Path(save_dir) / wandb_logger.version / "checkpoints"
+    checkpoint_callback = ModelCheckpoint(checkpoint_path, verbose=True, save_weights_only=True)
+
+    model = Transformer(langpair, is_base)
+    for p in model.parameters():
+        if p.dim() > 1:
+            nn.init.xavier_uniform_(p)
+
+    dataloader = WMT14DataLoader(langpair, is_base)
+
+    trainer = Trainer(
+        logger=wandb_logger,
+        checkpoint_callback=checkpoint_callback,
+        gpus=-1 if torch.cuda.is_available() else None,
+        auto_select_gpus=True,
+        log_gpu_memory="all",
+        check_val_every_n_epoch=1,
+        max_steps=config.model.train_hparams.steps,
+    )
+    trainer.fit(model, dataloader)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--langpair", '-lp', help="Language pair to translate", required=True)
+    parser.add_argument("--model", dest="model_type", choices=["base", "big"], required=True, default="base", help="Transformer model type (e.g., base, big)")
+
+    args = parser.parse_args()
+    train(args.langpair, args.model_type)
